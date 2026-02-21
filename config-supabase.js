6ca6d6e91442680d393540a55c4e53cf22224b12
marcosamontoya <106404893+marcosamontoya@users.noreply.github.com>
2026-02-21 12:29:39 -0300

    Change logo file extension from .jpeg to .jpg

--- config-supabase.js
+++ config-supabase.js
@@ -12,7 +12,7 @@ const SUPABASE_CONFIG = {
 
 // OPCIONAL: URL del logo de tu empresa
 // Ejemplo: const LOGO_URL = 'https://mi-sitio.com/logo.png';
-const LOGO_URL = '/main/AgroData360Logo.jpeg'; 
+const LOGO_URL = '/main/AgroData360Logo.jpg'; 
 
 // ============================================================
 // NO MODIFICAR DESDE AQUÍ HACIA ABAJO
